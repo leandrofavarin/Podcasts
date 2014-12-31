4b6c55741618a053bbe723a97370c16fb80ad722
Leandro Favarin <lfavarin@gmail.com>
2014-12-31 16:39:30 -0200

    Created proper menu file for directory Activity

--- app/src/main/java/com/leandrofavarin/podcasts/directory/DirectoryActivity.java
+++ app/src/main/java/com/leandrofavarin/podcasts/directory/DirectoryActivity.java
@@ -6,8 +6,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.android.common.view.SlidingTabLayout;
+import com.google.samples.apps.iosched.util.LogUtils;
 import com.leandrofavarin.podcasts.R;
 import com.leandrofavarin.podcasts.TitledFragment;
 
@@ -18,6 +21,8 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class DirectoryActivity extends ActionBarActivity {
+
+    private static final String TAG = LogUtils.makeLogTag(DirectoryActivity.class);
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
@@ -47,6 +52,30 @@ public class DirectoryActivity extends ActionBarActivity {
         viewPager.setCurrentItem(fragments.indexOf(topAudioFragment));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_directory, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_country_picker:
+                openCountryPicker();
+                return true;
+            case R.id.action_search:
+                // TODO
+                return true;
+            default:
+                LogUtils.LOGE(TAG, "Menu item id not recognized.");
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private List<TitledFragment> getFragments() {
         List<TitledFragment> fragments = new ArrayList<>();
 
@@ -57,5 +86,9 @@ public class DirectoryActivity extends ActionBarActivity {
         fragments.add(topAudioFragment);
 
         return fragments;
+    }
+
+    private void openCountryPicker() {
+
     }
 }
