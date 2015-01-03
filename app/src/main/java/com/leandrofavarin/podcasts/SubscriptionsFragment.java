27e5bc2ece2a5e702619868da3dc8a327f4dc053
Leandro Favarin <lfavarin@gmail.com>
2015-01-03 18:36:34 -0200

    Subscriptions list now uses Refreshable list with empty view

--- app/src/main/java/com/leandrofavarin/podcasts/SubscriptionsFragment.java
+++ app/src/main/java/com/leandrofavarin/podcasts/SubscriptionsFragment.java
@@ -8,14 +8,27 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.leandrofavarin.podcasts.directory.SimpleGridAdapter;
+import com.leandrofavarin.podcasts.utils.GridArtworksHelper;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class SubscriptionsFragment extends TitledFragment {
 
+    @InjectView(R.id.progress_bar)
+    ProgressBar progressBar;
+
     @InjectView(R.id.recycler_view)
     RecyclerView recyclerView;
+
+    @InjectView(android.R.id.empty)
+    TextView emptyView;
 
     public static SubscriptionsFragment newInstance() {
         return new SubscriptionsFragment();
@@ -26,13 +39,40 @@ public class SubscriptionsFragment extends TitledFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.view_refreshable_recyclerview, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_empty_refreshable_list, container, false);
         ButterKnife.inject(this, rootView);
         final Context context = rootView.getContext();
 
+        emptyView.setText(R.string.empty_subscriptions);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        updateList(context);
 
         return rootView;
+    }
+
+    private void updateList(Context context) {
+        GridArtworksHelper gridArtworksHelper = new GridArtworksHelper(context);
+        int columnWidth = gridArtworksHelper.getColumnSize();
+        SimpleGridAdapter simpleGridAdapter = new SimpleGridAdapter(context, new ArrayList<Podcast>(), columnWidth);
+        simpleGridAdapter.setEmptyView(emptyView);
+        recyclerView.setAdapter(simpleGridAdapter);
+
+        animateList();
+        animateProgress();
+    }
+
+    private void animateList() {
+        Context context = recyclerView.getContext();
+        long animDuration = context.getResources().getInteger(android.R.integer.config_mediumAnimTime);
+        recyclerView.setAlpha(0f);
+        recyclerView.animate().alpha(1f).setDuration(animDuration);
+    }
+
+    private void animateProgress() {
+        Context context = progressBar.getContext();
+        long animDuration = context.getResources().getInteger(android.R.integer.config_mediumAnimTime);
+        progressBar.setAlpha(1f);
+        progressBar.animate().alpha(0f).setDuration(animDuration);
     }
 
     @Override
