5f0e16c213d29a0e986f649c6254514272813266
Leandro Favarin <lfavarin@gmail.com>
2015-01-04 21:32:51 -0200

    Toolbar and protection for sent parameters to a podcast Activity

--- app/src/main/java/com/leandrofavarin/podcasts/PodcastActivity.java
+++ app/src/main/java/com/leandrofavarin/podcasts/PodcastActivity.java
@@ -2,8 +2,15 @@ package com.leandrofavarin.podcasts;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.google.samples.apps.iosched.util.LogUtils;
+
+import java.security.InvalidParameterException;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class PodcastActivity extends ActionBarActivity {
 
@@ -11,8 +18,27 @@ public class PodcastActivity extends ActionBarActivity {
 
     public static final String PODCAST_ID = "podcast_id";
 
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_podcast);
+        ButterKnife.inject(this);
+
+        Bundle extras = getIntent().getExtras();
+        if (!extras.containsKey(PODCAST_ID)) {
+            throw new InvalidParameterException("Intent does not contain key \"" + PODCAST_ID + "\"");
+        }
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
