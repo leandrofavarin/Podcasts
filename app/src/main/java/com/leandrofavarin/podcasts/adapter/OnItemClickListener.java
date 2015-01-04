1e088a562515edbc656d7abf0291c9be3380eafa
Leandro Favarin <lfavarin@gmail.com>
2015-01-04 20:25:13 -0200

    New OnItemClickListener for RecyclerViews

---
+++ app/src/main/java/com/leandrofavarin/podcasts/adapter/OnItemClickListener.java
@@ -0,0 +1,7 @@
+package com.leandrofavarin.podcasts.adapter;
+
+import android.view.View;
+
+public interface OnItemClickListener {
+    public abstract void onItemClick(View view, int position);
+}
