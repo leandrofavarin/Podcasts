85a6cd91e166e0c625e84d85ac1507c9698e6ee4
Leandro Favarin <lfavarin@gmail.com>
2015-01-08 00:06:05 -0200

    Updated directory Activity to work with the SearchView

--- app/src/main/java/com/leandrofavarin/podcasts/directory/DirectoryActivity.java
+++ app/src/main/java/com/leandrofavarin/podcasts/directory/DirectoryActivity.java
@@ -1,9 +1,12 @@
 package com.leandrofavarin.podcasts.directory;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.google.samples.apps.iosched.util.LogUtils;
 import com.leandrofavarin.podcasts.R;
@@ -13,7 +16,7 @@ import com.leandrofavarin.podcasts.TitledFragment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DirectoryActivity extends SwipeableActivity {
+public class DirectoryActivity extends SwipeableActivity implements SearchView.OnQueryTextListener {
 
     private static final String TAG = LogUtils.makeLogTag(DirectoryActivity.class);
 
@@ -50,6 +53,13 @@ public class DirectoryActivity extends SwipeableActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_directory, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(this);
+        searchView.setQueryHint(getString(R.string.search_hint));
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
@@ -61,7 +71,6 @@ public class DirectoryActivity extends SwipeableActivity {
                 openCountryPicker();
                 return true;
             case R.id.action_search:
-                // TODO
                 return true;
             default:
                 LogUtils.LOGE(TAG, "Menu item id not recognized.");
@@ -73,5 +82,15 @@ public class DirectoryActivity extends SwipeableActivity {
 
     private void openCountryPicker() {
 
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
