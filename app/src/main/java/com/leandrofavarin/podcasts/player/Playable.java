c1cdb7f1f717f7bd7790ffa23a337ca762b145e2
Leandro Favarin <lfavarin@gmail.com>
2015-01-10 21:10:36 -0200

    Declare interface for Playables

---
+++ app/src/main/java/com/leandrofavarin/podcasts/player/Playable.java
@@ -0,0 +1,9 @@
+package com.leandrofavarin.podcasts.player;
+
+public interface Playable {
+    public abstract String getTitle();
+
+    public abstract String getArtist();
+
+    public abstract String getUrl();
+}
