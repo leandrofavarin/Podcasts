5ec35d7128277f7b33811ff27fbd3dca99157b83
Leandro Favarin <lfavarin@gmail.com>
2015-01-12 00:18:38 -0200

    Extend directory adapter from TitledFragmentsPagerAdapter

--- app/src/main/java/com/leandrofavarin/podcasts/directory/DirectoryPagerAdapter.java
+++ app/src/main/java/com/leandrofavarin/podcasts/directory/DirectoryPagerAdapter.java
@@ -1,41 +1,21 @@
 package com.leandrofavarin.podcasts.directory;
 
 import android.content.Context;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 
 import com.leandrofavarin.podcasts.R;
 import com.leandrofavarin.podcasts.TitledFragment;
+import com.leandrofavarin.podcasts.TitledFragmentsPagerAdapter;
 
 import java.util.List;
 
-public class DirectoryPagerAdapter extends FragmentPagerAdapter {
+public class DirectoryPagerAdapter extends TitledFragmentsPagerAdapter {
 
-    private Context context;
-    private List<TitledFragment> fragments;
     private boolean isLargeScreen;
 
     public DirectoryPagerAdapter(Context context, FragmentManager fManager, List<TitledFragment> items) {
-        super(fManager);
-        this.context = context;
-        this.fragments = items;
+        super(context, fManager, items);
         this.isLargeScreen = context.getResources().getBoolean(R.bool.largeScreen);
-    }
-
-    @Override
-    public int getCount() {
-        return fragments.size();
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return fragments.get(position).getTitle(context);
-    }
-
-    @Override
-    public Fragment getItem(int position) {
-        return fragments.get(position);
     }
 
     @Override
