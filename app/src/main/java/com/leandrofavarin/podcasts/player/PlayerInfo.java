c06bb8f6c9416d4180a3941457ffe3f65666935d
Leandro Favarin <lfavarin@gmail.com>
2015-01-27 02:37:07 -0200

    Add Map containing public player information

---
+++ app/src/main/java/com/leandrofavarin/podcasts/player/PlayerInfo.java
@@ -0,0 +1,16 @@
+package com.leandrofavarin.podcasts.player;
+
+import java.util.HashMap;
+
+public class PlayerInfo extends HashMap<String, Object> {
+
+    /**
+     * Duration in Millis
+     */
+    public static final String DURATION = "duration";
+
+    /**
+     * Current Position in Millis
+     */
+    public static final String CURRENT_POSITION = "current_position";
+}
